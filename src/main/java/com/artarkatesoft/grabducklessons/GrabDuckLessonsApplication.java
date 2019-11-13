d5685fe51d23e06f681e0aadd358df301ec16a6d
admin <d.art.shishkin@gmail.com>
2019-11-13 16:19:12 +0200

    First commit

---
+++ src/main/java/com/artarkatesoft/grabducklessons/GrabDuckLessonsApplication.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.grabducklessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabDuckLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabDuckLessonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artarkatesoft/grabducklessons/GrabDuckLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.grabducklessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabDuckLessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
